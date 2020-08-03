166d22cf1dab246d27b7106ba1da168cb8b3e80b
Enrique Davila <Enrique.Davila@gmail.com>
2020-08-03 14:47:20 -0500

    New feature added: get diff minutes

--- incidentClass.py
+++ incidentClass.py
@@ -28,4 +28,15 @@ class incident:
         else:
           return 0
         return result
+
+    def getDiffMins(self):
+        date1 = selfStartDate
+        date2 = self.endDate
+        date1Aux = datetime.strptime(date1, '%m/%d/%y %H:%M:%S')
+        date2Aux = datetime.strptime(date2, '%m/%d/%y %H:%M:%S')
+        if date1Aux > date2Aux:
+          result = date1Aux - date2Aux
+        else:
+          return 0
+        return result
   
