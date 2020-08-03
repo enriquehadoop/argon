480c0a08486644209e901d8c5702e7d764b71fc0
Enrique Davila <Enrique.Davila@gmail.com>
2020-08-03 16:06:11 -0500

    New method to calculate by hrs

--- incidentClass.py
+++ incidentClass.py
@@ -39,4 +39,14 @@ class incident:
         else:
           return 0
         return result
+    def getDiffMinsHrs(self):
+        date1 = selfStartDate
+        date2 = self.endDate
+        date1Aux = datetime.strptime(date1, '%m/%d/%y %H:%M:%S')
+        date2Aux = datetime.strptime(date2, '%m/%d/%y %H:%M:%S')
+        if date1Aux > date2Aux:
+          result = date1Aux - date2Aux
+        else:
+          return 0
+        return result
   
